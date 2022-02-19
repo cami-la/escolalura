8c9065aeb792f27171f4471a1e0579db375817e6
Camila Cavalcante <camiladsantoscavalcante@gmail.com>
2022-02-19 19:05:40 -0300

    Initial commit

---
+++ src/main/java/br/com/alura/escolalura/EscolaluraApplication.java
@@ -0,0 +1,13 @@
+package br.com.alura.escolalura;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EscolaluraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EscolaluraApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/alura/escolalura/EscolaluraApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.alura.escolalura;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EscolaluraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
